98b7151d4d87b881d28e2a43c31deb1278052eb4
tank3ngine <117690704+tank3ngine@users.noreply.github.com>
2023-08-24 03:00:06 -0400

    qwer

--- Brackeys 2023 part two/Assets/CloudScript.cs
+++ Brackeys 2023 part two/Assets/CloudScript.cs
@@ -6,16 +6,27 @@ public class CloudScript : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private bool movingRight;
+    [SerializeField] private float spawnPosX;
+    [SerializeField] private float spawnPosY;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnPosX = transform.position.x;
+        spawnPosY = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.position.x = transform.position.x + ;
+        transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y);
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+         if (collision.CompareTag("Killzone"))
+        {
+            transform.position = new Vector3(spawnPosX, spawnPosY, 0f);
+            Debug.Log("killzone found");
+        }
     }
 }
